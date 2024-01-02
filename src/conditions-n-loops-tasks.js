1673f8f254df9f41282d0b4f384a969977fcb131
Vital Kabantsou <Vitali86@tut.by>
2024-01-02 16:41:25 +0300

    feat: isContainNumber

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -265,8 +265,14 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const numText = String(num);
+  for (let i = 0; i < numText.length; i += 1) {
+    if (numText[i] === String(digit)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 /**
